b52c2158661b29d4c048928467374a118b91a173
Pablo J. Veintimilla Vargas <pablo.veintimilla@ppm.com.ec>
2022-08-06 12:44:45 -0500

    Add effor column in category

---
+++ Migrations/20220806174102_01-category-add-column-effor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace entityframework.Migrations
+{
+    public partial class _01categoryaddcolumneffor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Effor",
+                table: "Category",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Effor",
+                table: "Category");
+        }
+    }
+}
--- Models/Category.cs
+++ Models/Category.cs
@@ -13,6 +13,9 @@ namespace entityframework.Models
         [MaxLength(150)]
         public string? Name { get; set; }
         public string? Description { get; set; }
+
+        public int Effor { get; set; }
+
         public virtual ICollection<Task>? Tasks { get; set; }
     }
 }
